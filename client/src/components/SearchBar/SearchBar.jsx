2d135ef93dc26bb380a50fa87afc3914b0456e04
MiYazJE <ruben.saizserrano@gmail.com>
2020-08-03 08:45:52 +0200

    Change video

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { searchVideoSuggestions, searchYoutubeVideos } from '../../Http/api';
 import ReactPlayer from 'react-player';
 import './searchBar.scss';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 const INITIAL_URL_VIDEO = 'https://www.youtube.com/watch?v=A1xEete-zHM';
 
 const Video = ({ title, urlThumbnail, url, changeVideo }) => (
     <div className="video">
         <div className="top">
-            <img onClick={() => changeVideo(`https://youtube.com/${url}`)} src={urlThumbnail}/>
+            <img onClick={() => changeVideo(`https://youtube.com${url}`)} src={urlThumbnail}/>
         </div>
         <div className="bottom">
             <p className="title">{title}</p>
@@ -20,43 +21,55 @@ const Video = ({ title, urlThumbnail, url, changeVideo }) => (
 );
 
 const SearchBar = () => {
-    const [urlVideo, setUrlVideo] = useState('')
+    const [loading, setLoading] = useState(false);
+    const [urlVideo, setUrlVideo] = useState(INITIAL_URL_VIDEO)
     const [videosSuggested, setVideosSuggested] = useState([]);
     const [videos, setVideos] = useState([]);
 
-    const searchAutoCompletation = async ({ target }) => {
-        if (!target.value) {
+    const refResultVideos = useRef();
+
+    const searchAutoCompletation = async (value) => {
+        if (!value) {
             setVideosSuggested([]);
             return;
         };
-        const data = await searchVideoSuggestions(target.value);
+        const data = await searchVideoSuggestions(value);
         setVideosSuggested([...data]);
     };
 
     const getVideos = async (searched) => {
+        setLoading(true);
+        searchAutoCompletation(searched);
         const videos = await searchYoutubeVideos(searched);
         setVideos(videos);
+        setLoading(false);
+        scroll(refResultVideos);
     }
 
     const changeVideo = (url) => {
         setUrlVideo(url);
     }
 
+    const scroll = (ref) => ref.current.scrollIntoView({ behavior: 'smooth' });
+
     return (
         <div id="wrapVideoPlayer">
-            <Autocomplete
-                onChange={(_, searched) => getVideos(searched)}
-                style={{ width: '70%', maxWidth: 350 }}
-                options={videosSuggested}
-                renderInput={(params) => (
-                    <Input {...params} onChange={searchAutoCompletation} id="search" placeholder="Search a video" />
-                )}
-                noOptionsText="No results"
-            />
+            <div className="wrapSearchBar">
+                <Autocomplete
+                    onChange={(_, searched) => getVideos(searched)}
+                    style={{ width: '70%', maxWidth: 350 }}
+                    options={videosSuggested}
+                    renderInput={(params) => (
+                        <Input {...params} onChange={({ target }) => searchAutoCompletation(target.value)} id="search" placeholder="Search a video" />
+                    )}
+                    noOptionsText="No results"
+                />
+                {loading ? <CircularProgress style={{marginLeft: '30px'}} size={30} /> : null}
+            </div>
             <div className="reactPlayer">
                 <ReactPlayer width="100%" height="100%" controls={true} url={urlVideo} />
             </div>
-            <div className="videosContainer">
+            <div ref={refResultVideos} className="videosContainer">
                 {videos.map(video => <Video key={video.url} changeVideo={changeVideo} {...video} />)}
             </div>
         </div>
