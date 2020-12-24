02c0f800a5b53ce58e6f8400aa74cd22355af04c
MiYazJE <ruben.saizserrano@gmail.com>
2020-12-25 00:57:24 +0100

    config for production

--- db/config.js
+++ db/config.js
@@ -5,12 +5,12 @@ let user     = 'ruben';
 let password = 'root';
 let database = 'share_videos';
 
-// if (process.env.ENVIROMENT === 'production') {
+if (process.env.ENVIROMENT === 'production') {
     host     = process.env.DB_HOST;
     user     = process.env.DB_USER;
     password = process.env.DB_PASSWORD;
     database = process.env.DB_DATABASE_NAME;
-// }
+}
 
 const config = {
     host,
