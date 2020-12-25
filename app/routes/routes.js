8faa2b5a96f6af4a20c50c135e7e9de8383367a3
MiYazJE <ruben.saizserrano@gmail.com>
2020-12-25 04:36:17 +0100

    login register whoAmI

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
+const passport = require('passport');
 const userCtrl = require('../controllers/users.controller');
+const authCtrl = require('../controllers/auth.controller');
 const roomsCtrl = require('../controllers/rooms.controller');
 
-router.post('/user/register', userCtrl.register);
-router.post('/user/login',    userCtrl.login);
+router.post('/user/register', authCtrl.register);
+router.post('/user/login', (req, res, next) =>
+    passport.authenticate(
+        'local-login',
+        { session: false },
+        authCtrl.login(req, res)
+    )(req, res, next)
+);
+
+router.get('/auth/whoAmI', passport.authenticate('jwt', { session: false }), authCtrl.whoAmI);
 
 router.get('/search/autocomplete/:q', userCtrl.searchVideoSuggestions);
-router.get('/youtube/:q',             userCtrl.getVideos);
+router.get('/youtube/:q', userCtrl.getVideos);
 
-router.post('/room/create',     roomsCtrl.create);
+router.post('/room/create', roomsCtrl.create);
 router.get('/room/isValid/:id', roomsCtrl.isValid);
 
 module.exports = router;
