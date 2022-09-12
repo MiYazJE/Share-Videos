aea1f97937f0f92d2fb3b8ff70bd1a1acd9141df
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2022-09-12 09:34:46 +0200

    fix(server): remove token validations

--- server/app/api/rooms/rooms.router.js
+++ server/app/api/rooms/rooms.router.js
@@ -6,7 +6,7 @@ const validation = require('./rooms.validation');
 const { userAuthMiddleware } = require('../../middlewares/authMiddlewares');
 const { validateSchema } = require('../../middlewares/validation');
 
-router.post('/create', userAuthMiddleware, validateSchema(validation.create), controller.create);
-router.get('/:id/isValid/', userAuthMiddleware, controller.isValid);
+router.post('/create', validateSchema(validation.create), controller.create);
+router.get('/:id/isValid/', controller.isValid);
 
 module.exports = router;
--- server/app/api/videos/videos.router.js
+++ server/app/api/videos/videos.router.js
@@ -8,14 +8,12 @@ const { validateSchema } = require('../../middlewares/validation');
 
 router.get(
   '/autocomplete/youtube/:q',
-  userAuthMiddleware,
   validateSchema(validation.searchParam),
   controller.autocompleteYoutube,
 );
 
 router.get(
   '/youtube/:q',
-  userAuthMiddleware,
   validateSchema(validation.searchParam),
   controller.getYoutubeVideos,
 );
